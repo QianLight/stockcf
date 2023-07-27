77c04ac4ee6f92adac690c78d024b2cf6c49b280
QianLight <2235788727@qq.com>
2023-07-28 00:20:11 +0800

    形态算法更新

--- instock/core/strategy/klinesimilar.py
+++ instock/core/strategy/klinesimilar.py
@@ -24,6 +24,11 @@ def check_klinesimilar(comparedata,code_name, data, date=None, threshold=60):
     #data = data.tail(n=threshold)
     corrcoef.caculatema5(data)
 
+    maxk=-2
+    mink=100
+    desmax=""
+    desmin = ""
+
     for compareStocks in comparedata:
         targetlength=len(compareStocks)
         if len(data)<targetlength:
@@ -31,8 +36,15 @@ def check_klinesimilar(comparedata,code_name, data, date=None, threshold=60):
         datatoday = data.tail(n=targetlength)
         corrcoef.caculateCorrcoefData(datatoday)
         ave_k =corrcoef.caculateCorrcoef(compareStocks,datatoday)
-        if ave_k > 0.7 or ave_k <-0.7:
-            des=compareStocks.iloc[0]["dynamic_para"]
-            dynamic_parastr=f"{round(ave_k+1, 2)}-{des}"
-            return True,dynamic_parastr
+        if ave_k>maxk:
+            maxk=ave_k
+            desmax = compareStocks.iloc[0]["dynamic_para"]
+
+        if ave_k < mink:
+            mink=ave_k
+            desmin = compareStocks.iloc[0]["dynamic_para"]
+
+    if maxk > 0.7 or mink <-0.7:
+        dynamic_parastr=f"{round(maxk+1, 2)}-{desmax}-{round(mink+1, 2)}-{desmin}"
+        return True,dynamic_parastr
     return False
