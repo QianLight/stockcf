84b24b85e22cbd3b3cb1199a9d6b038ba3ab7e78
QianLight <2235788727@qq.com>
2023-07-30 18:30:30 +0800

    指标回测

--- instock/core/strategy/early_support_position.py
+++ instock/core/strategy/early_support_position.py
@@ -22,23 +22,125 @@ def check(code_name, data, date=None, threshold=60):
         data = data.tail(n=threshold)
         data.reset_index(inplace=True, drop=True)
 
+    result=checklow60(data)
+    return result
+
+def checklowup10(data):
+    mask = (data['p_change'] >9.5)
+    dataup10 = data.loc[mask].copy()
+    if len(dataup10)<1:
+        return False
+
     daymin = data.iloc[-1]['low']
+
+    minallTime = dataup10['low'].values.min()
+
+    daymin_low_ratio = round((daymin - minallTime) / minallTime, 2)
+    low_index = data['low'].idxmin() + 1
+    daymin_low_ratio_day = len(data) - low_index
+
 
     maxallTime = data['high'].values.max()
     daymin_max_ratio = round((maxallTime - daymin) / maxallTime, 2)
     max_index = data['high'].idxmax() + 1
     daymin_max_ratio_day = len(data) - max_index
 
+    desshow = f"{daymin_low_ratio}:{daymin_low_ratio_day},{daymin_max_ratio}:{daymin_max_ratio_day}"
+    if daymin_low_ratio <0.05:
+        return True, desshow
+
+    return False
+
+
+def checkHasWave(data, upchange=10, waveminchange=4):
+    regionwavechange_down = 0
+    regionwavechange_up = 0
+
+    regionwavestarthigh = 0
+    regionwavechange = 0
+    changeindex = 0
+
+
+    isup = False
+
+    wavehigh = []
+    wavelow = []
+
+    wavehighindex = []
+    wavelowindex = []
+
+    wavestartindex = len(data) -1
+
+    waveindex = len(data) - 2
+    while waveindex >= 0:
+        #currentlow= data['low'].values[startindex]
+
+        if  data['high'].values[waveindex]>data['high'].values[wavestartindex]:
+            regionwavechange = (data['high'].values[waveindex] - data['low'].values[wavestartindex]) / data['low'].values[wavestartindex]
+        else:
+            regionwavechange = (data['high'].values[wavestartindex] - data['low'].values[waveindex]) / \
+                               data['low'].values[wavestartindex]
+
+        if regionwavechange > 0:
+            if regionwavechange > regionwavechange_up:  # 持续上涨 记录上涨幅度
+                regionwavechange_up = regionwavechange
+            else:  # 假如不上涨 记录开始的时间
+                changeindex = waveindex
+                if regionwavechange_up - regionwavechange > waveminchange:  # 假如距离上次涨幅超过了小波震动 估计已经反转了
+                    wavehighindex.append(changeindex + 1)
+                    wavehigh.append(regionwavechange_up)
+                    regionwavestartlow = data['low'].values[waveindex]
+                    regionwavechange_up = 0
+
+        elif regionwavechange < 0:
+            if regionwavechange < regionwavechange_down:  # 持续下跌 记录上涨幅度
+                regionwavechange_down = regionwavechange
+            else:  # 假如不下跌 记录开始的时间
+                changeindex = waveindex
+                if regionwavechange - regionwavechange_down > waveminchange:  # 假如距离上次下跌超过了小波震动 估计已经反转了
+                    wavelowindex.append(changeindex + 1)
+                    wavelow.append(regionwavechange_down)
+                    regionwavestartlow = data['low'].values[waveindex]
+                    regionwavechange_down = 0
+
+        waveindex -= 1
+
+    if regionwavechange_up > 0:
+        wavehigh.append(regionwavechange_up)
+    elif regionwavechange_up < 0:
+        wavelow.append(regionwavechange_up)
+
+    return False
+
+def checklow60(data):
+    mask = (data['p_change'] >8)
+    dataup5 = data.loc[mask].copy()
+    if len(dataup5)<1:
+        return False
+
+    #datahigh=data.sort_values(by="high", inplace=False, ascending=True)
+    #datalow = data.sort_values(by="low", inplace=False, ascending=True)
+
+    daymin = data.iloc[-1]['low']
+
     minallTime = data['low'].values.min()
+
     daymin_low_ratio = round((daymin - minallTime) / daymin, 2)
     low_index = data['low'].idxmin() + 1
     daymin_low_ratio_day = len(data) - low_index
+
+
+    maxallTime = data['high'].values.max()
+    daymin_max_ratio = round((maxallTime - daymin) / maxallTime, 2)
+    max_index = data['high'].idxmax() + 1
+    daymin_max_ratio_day = len(data) - max_index
 
     desshow = f"{daymin_low_ratio}:{daymin_low_ratio_day},{daymin_max_ratio}:{daymin_max_ratio_day}"
     if daymin_low_ratio <0.05:
         return True, desshow
 
     return False
+
 
 def check1(code_name, data, date=None, threshold=60):
     if date is None:
