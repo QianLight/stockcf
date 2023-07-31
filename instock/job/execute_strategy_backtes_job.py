fed1ee2f9989ceb022a2a9a86dae5311110bf866
QianLight <2235788727@qq.com>
2023-07-31 23:55:46 +0800

    数据修改 添加注释

--- instock/job/execute_strategy_backtes_job.py
+++ instock/job/execute_strategy_backtes_job.py
@@ -46,7 +46,7 @@ def main():
         # # # # 第4步创建股票k线形态表
         #executor.submit(kdj.main)
         # # # # 第5步创建股票策略数据表
-        executor.submit(sdj.main,0)
+        executor.submit(sdj.main)
 
     # # # # 第6步创建股票回测
     bdj.main(0)
